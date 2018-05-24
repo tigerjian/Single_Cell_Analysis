cc8039158c7bbda56d9a31dc9173c85674f8b37c
Gwen <gwenm97@gmail.com>
2018-05-24 14:53:10 -0400

    Merge branch 'master' of https://github.com/tigerjian/Single_Cell_Analysis

--- Source/file.py
+++ Source/file.py
@@ -1,15 +1,16 @@
 import os
 
-DAPI_file = "DAPI_a_tubulin_pattern_1_R3D_PRJ_w435_t%.3d.tif"
-a_tubulin_file = "DAPI_a_tubulin_pattern_1_R3D_PRJ_w632_t%.3d.tif"
-pattern_file = "DAPI_a_tubulin_pattern_1_R3D_PRJ_w676_t%.3d.tif"
-low_res_log = "DAPI_a_tubulin_pattern_1_R3D.dv.log"
+DAPI_file = "DAPI_pattern_20x_test_1_R3D_PRJ_w435_t%.3d.tif"
+# a_tubulin_file = "DAPI_a_tubulin_pattern_1_R3D_PRJ_w632_t%.3d.tif"
+pattern_file = "DAPI_pattern_20x_test_1_R3D_PRJ_w676_t%.3d.tif"
+low_res_log = "DAPI_pattern_20x_test_1_R3D.dv.log"
 
-num_low_res = 260
+num_low_res = 110
 
 def get_low_res_coord():
     parent = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-    path = os.path.join(parent, "Low_Res_Input_Images", low_res_log)
+    # path = os.path.join(parent, "Low_Res_Input_Images", low_res_log) # for 10x imaging
+    path = os.path.join(parent, "Low_Res_Input_Images_20x", low_res_log) # for 20x imaging
     
     file = open(path, "r")
     file_lines = file.readlines()
